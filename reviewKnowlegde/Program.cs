7324990b50f4d2c7df9ba083f694cbfa4f38f883
nguyenvinh.nguyen <nguyenvinh.nguyen@petronas.com>
2022-04-11 19:58:58 +0700

    the message commit for gitignore

---
+++ reviewKnowlegde/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace reviewKnowlegde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
